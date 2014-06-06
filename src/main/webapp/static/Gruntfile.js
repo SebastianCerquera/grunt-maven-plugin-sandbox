995615aeb19c06e5b8e097899faa1955a58416f9
beto <beto@localhost.localdomain>
2014-06-06 14:15:00 -0500

    wtp support

--- src/main/webapp/static/Gruntfile.js
+++ src/main/webapp/static/Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+    	
         pkg: grunt.file.readJSON('package.json'),
+        
         karma: {
             unit: {
                 options: {
@@ -12,13 +14,20 @@ module.exports = function(grunt) {
                 }
             }
         },
+        
         gruntMavenProperties: grunt.file.readJSON('grunt-maven.json'),
+        
         mavenPrepare: {
             options: {
                 resources: ['**']
             },
             dev: {}
         },
+        
+        /*
+         * If wtp integration is required, due to the packagingExcludes the final war does not
+         * contain the code from dist.
+         */
         mavenDist: {
             options: {
                 warName: 'ROOT',
@@ -26,16 +35,33 @@ module.exports = function(grunt) {
             },
             dev: {}
         },
+        
+        /*
+         
+        If wtp integration is required, it's necesary to copy bower_components and test. 
+         
+		copy : {
+			main : {
+				files : [ {
+					src : [ "dist/test/**", "dist/bower_components/**" ],
+					dest : 'path to server'
+				} ]
+			}
+		},
+		
+		*/
+        
         watch: {
             files: ["<%= gruntMavenProperties.filesToWatch %>"],
             tasks: ['default']
         }
+        
     });
 
     grunt.loadNpmTasks('grunt-maven');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-karma');
 
-    grunt.registerTask('default', ['mavenPrepare', 'karma', 'mavenDist']);
+    grunt.registerTask('default', ['mavenPrepare', 'karma', 'mavenDist', /*copy:main*/]);
 
 };
